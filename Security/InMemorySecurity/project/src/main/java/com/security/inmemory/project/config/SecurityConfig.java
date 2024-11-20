79b6e3822f2929d6677e085b830fc55fe2a70870
Ved-avid <vedavid2505@gmail.com>
2024-11-20 22:40:46 +0530

    added security chain details

--- Security/InMemorySecurity/project/src/main/java/com/security/inmemory/project/config/SecurityConfig.java
+++ Security/InMemorySecurity/project/src/main/java/com/security/inmemory/project/config/SecurityConfig.java
@@ -2,19 +2,21 @@ package com.security.inmemory.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
     // CREATING IN MEMORY USER FOR AUTHENTICATION
-    
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService(){
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -23,5 +25,19 @@ public class SecurityConfig{
                            roles("USER").build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    // FOR SECURITY CHECKS
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests().
+                requestMatchers("/user").authenticated()
+                .requestMatchers("/home").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+        return http.build();
     }
 }
