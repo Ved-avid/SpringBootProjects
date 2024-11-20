e77cf66fe8c715a878c69a3a260016c23096bddb
Ved-avid <vedavid2505@gmail.com>
2024-11-20 22:51:26 +0530

    added API

--- Security/InMemorySecurity/project/src/main/java/com/security/inmemory/project/ProjectApplication.java
+++ Security/InMemorySecurity/project/src/main/java/com/security/inmemory/project/ProjectApplication.java
@@ -3,12 +3,27 @@ package com.security.inmemory.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+	}
+	@GetMapping("/test")
+	public String test(){
+		return "Test API is working";
+	}
+	@GetMapping("/user")
+	public String userTest(){
+		return "User API is working";
+	}
+	@GetMapping("/home")
+	public String home(){
+		return "Home API is working";
 	}
 
 }
