725447856d22370a0cf8954e397da8ea3cf23b19
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2024-12-09 12:26:16 +0530

    starting major project

---
+++ Security/FinalProject/major_pproject/src/main/java/com/ved/major_pproject/MajorPprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ved.major_pproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorPprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorPprojectApplication.class, args);
+	}
+
+}
---
+++ Security/FinalProject/major_pproject/src/test/java/com/ved/major_pproject/MajorPprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ved.major_pproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorPprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
