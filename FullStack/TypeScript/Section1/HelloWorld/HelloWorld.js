51aa90abe86ea0558c4bc3b14eea87966d68d595
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2024-12-18 10:23:10 +0530

    Compilation of typeScript files

--- FullStack/TypeScript/Section1/HelloWorld/HelloWorld.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello World from TypeScript Syntax");
-console.log("Coding is fun" + ", " + "Code makes life so beautiful");
-var firstName = "Ved";
-var lastName = "Singh";
-console.log("Hi, ".concat(firstName, " ").concat(lastName));
--- FullStack/TypeScript/Section1/HelloWorld/HelloWorld.ts
+++ FullStack/TypeScript/Section1/HelloWorld/HelloWorld.ts
@@ -4,4 +4,10 @@ console.log("Coding is fun" + ", " + "Code makes life so beautiful");
 let firstName: string = "Ved";
 let lastName: string = "Singh";
 
+//firstName = 1;
+
+// If want to compile only when there is no error in the typeScript then please use the below command for compilation
+// tsc -noEmitOnError sample.ts
+// The above command does not create the js file when there is any error in the compilation of typeScript
+
 console.log(`Hi, ${firstName} ${lastName}`);
