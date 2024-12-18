aaa35eeab240807a1e57fa13cd9873bfd824eb7f
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2024-12-18 18:02:30 +0530

    some small chages in the application

--- FullStack/Angular-training/Session2/my-first-angular-project/src/app/app.component.ts
+++ FullStack/Angular-training/Session2/my-first-angular-project/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'appByDefault-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'Angulat project';
 }
