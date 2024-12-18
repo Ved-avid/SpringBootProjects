12d354fd5c4d0764d5461ec91d328d9207337e0d
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2024-12-18 10:16:01 +0530

    TypeScript

---
+++ FullStack/TypeScript/Section1/HelloWorld/HelloWorld.js
@@ -0,0 +1,5 @@
+console.log("Hello World from TypeScript Syntax");
+console.log("Coding is fun" + ", " + "Code makes life so beautiful");
+var firstName = "Ved";
+var lastName = "Singh";
+console.log("Hi, ".concat(firstName, " ").concat(lastName));
---
+++ FullStack/TypeScript/Section1/HelloWorld/HelloWorld.ts
@@ -0,0 +1,7 @@
+console.log("Hello World from TypeScript Syntax");
+console.log("Coding is fun" + ", " + "Code makes life so beautiful");
+
+let firstName: string = "Ved";
+let lastName: string = "Singh";
+
+console.log(`Hi, ${firstName} ${lastName}`);
