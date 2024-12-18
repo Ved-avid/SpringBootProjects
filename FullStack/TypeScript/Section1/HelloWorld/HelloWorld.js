e6bb64ceed55154189d4596f3578abe19d75c8e4
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2024-12-18 10:54:47 +0530

    Arrays and Loops in type Script

---
+++ FullStack/TypeScript/Section1/HelloWorld/HelloWorld.js
@@ -0,0 +1,32 @@
+console.log("Hello World from TypeScript Syntax");
+console.log("Coding is fun" + ", " + "Code makes life so beautiful");
+var firstName = "Ved";
+var lastName = "Singh";
+//firstName = 1;
+// If want to compile only when there is no error in the typeScript then please use the below command for compilation
+// tsc -noEmitOnError sample.ts
+// The above command does not create the js file when there is any error in the compilation of typeScript
+console.log("Hi, ".concat(firstName, " ").concat(lastName));
+var reviews = [4, 2, 3.5, 5, 1];
+// Printing the reviews
+for (var i = 0; i < reviews.length; i++) {
+    console.log(reviews[i]);
+}
+// Computing the average
+var total = 0;
+for (var i = 0; i < reviews.length; i++) {
+    total += reviews[i];
+}
+var averageOfReviews = total / reviews.length;
+console.log("The Avergae of the movie reviews is ".concat(averageOfReviews));
+// Other way for FOR LOOP
+var sports = ["Cricket", "VolleyBall", "FootBall"];
+for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
+    var tempSport = sports_1[_i];
+    if (tempSport == "Cricket") {
+        console.log("My favorite sports is ".concat(tempSport));
+    }
+    else {
+        console.log(tempSport);
+    }
+}
--- FullStack/TypeScript/Section1/HelloWorld/HelloWorld.ts
+++ FullStack/TypeScript/Section1/HelloWorld/HelloWorld.ts
@@ -11,3 +11,30 @@ let lastName: string = "Singh";
 // The above command does not create the js file when there is any error in the compilation of typeScript
 
 console.log(`Hi, ${firstName} ${lastName}`);
+
+let reviews: number[] = [4, 2, 3.5, 5, 1];
+
+// Printing the reviews
+for (let i = 0; i < reviews.length; i++) {
+  console.log(reviews[i]);
+}
+
+// Computing the average
+let total: number = 0;
+for (let i = 0; i < reviews.length; i++) {
+  total += reviews[i];
+}
+
+let averageOfReviews = total / reviews.length;
+console.log(`The Avergae of the movie reviews is ${averageOfReviews}`);
+
+// Other way for FOR LOOP
+
+let sports: string[] = ["Cricket", "VolleyBall", "FootBall"];
+for (let tempSport of sports) {
+  if (tempSport == "Cricket") {
+    console.log(`My favorite sports is ${tempSport}`);
+  } else {
+    console.log(tempSport);
+  }
+}
